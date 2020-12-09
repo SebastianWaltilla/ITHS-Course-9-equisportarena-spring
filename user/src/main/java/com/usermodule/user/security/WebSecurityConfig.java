9a3bb176d81fe444c5df7be4e92ff499627f6c14
Skoorps <skoorps@gmail.com>
2020-12-09 16:34:25 +0100

    hardcoded jwt token works

--- user/src/main/java/com/usermodule/user/security/WebSecurityConfig.java
+++ user/src/main/java/com/usermodule/user/security/WebSecurityConfig.java
@@ -1,17 +1,46 @@
 package com.usermodule.user.security;
 
+import com.usermodule.user.jwt.JwtAuthenticationEntryPoint;
+import com.usermodule.user.jwt.JwtRequestFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.*;
 import org.springframework.security.config.annotation.authentication.builders.*;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Autowired
+    private UserDetailsService jwtUserDetailsService;
+
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        // configure AuthenticationManager so that it knows from where to load
+        // user for matching credentials
+        // Use BCryptPasswordEncoder
+        auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -23,36 +52,56 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService());
-        authProvider.setPasswordEncoder(passwordEncoder());
-
-        return authProvider;
-    }
-
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        // We don't need CSRF for this example
+        httpSecurity.csrf().disable()
+                // dont authenticate this particular request
+                .authorizeRequests().antMatchers("/authenticate").permitAll().
+                // all other requests need to be authenticated
+                        anyRequest().authenticated().and().
+                // make sure we use stateless session; session won't be used to
+                // store user's state.
+                        exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        // Add a filter to validate the tokens with every request
+        httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/").hasAnyAuthority("USER", "CREATOR", "EDITOR", "ADMIN")
-                .antMatchers("/new").hasAnyAuthority("ADMIN", "CREATOR")
-                .antMatchers("/edit/**").hasAnyAuthority("ADMIN", "EDITOR")
-                .antMatchers("/delete/**").hasAuthority("ADMIN")
-                .antMatchers("/anyPage").hasAuthority("ADMIN")
-                .antMatchers("/admin/**").hasAuthority("ADMIN")
-                .anyRequest().authenticated()
-                .and()
-                .formLogin().permitAll()
-                .and()
-                .logout().permitAll()
-                .and()
-                .exceptionHandling().accessDeniedPage("/403")
-        ;
-    }
+
+
+
+   //@Bean
+   //public DaoAuthenticationProvider authenticationProvider() {
+   //    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+   //    authProvider.setUserDetailsService(userDetailsService());
+   //    authProvider.setPasswordEncoder(passwordEncoder());
+
+   //    return authProvider;
+   //}
+
+   //@Override
+   //protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+   //    auth.authenticationProvider(authenticationProvider());
+   //}
+
+    //@Override
+    //protected void configure(HttpSecurity http) throws Exception {
+    //    http.authorizeRequests()
+    //            .antMatchers("/").hasAnyAuthority("USER", "CREATOR", "EDITOR", "ADMIN")
+    //            .antMatchers("/new").hasAnyAuthority("ADMIN", "CREATOR")
+    //            .antMatchers("/edit/**").hasAnyAuthority("ADMIN", "EDITOR")
+    //            .antMatchers("/delete/**").hasAuthority("ADMIN")
+    //            .antMatchers("/anyPage").hasAuthority("ADMIN")
+    //            .antMatchers("/admin/**").hasAuthority("ADMIN")
+    //            .anyRequest().authenticated()
+    //            .and()
+    //            .formLogin().permitAll()
+    //            .and()
+    //            .logout().permitAll()
+    //            .and()
+    //            .exceptionHandling().accessDeniedPage("/403")
+    //    ;
+    //}
 }
