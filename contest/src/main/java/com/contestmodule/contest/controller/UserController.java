c9de1bf4fdd230e94e78e4e56db1ff3de48459b8
Linus Selvén <l.selven97@gmail.com>
2020-12-15 16:00:52 +0100

    Added validation to email field of users and implemented the change to the user created on startup. Controller class also throws more detailed errors instead of 500.

--- contest/src/main/java/com/contestmodule/contest/controller/UserController.java
+++ contest/src/main/java/com/contestmodule/contest/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.contestmodule.contest.controller;
 
 
+import com.contestmodule.contest.Exceptions.UserAlreadyExistsException;
+import com.contestmodule.contest.Exceptions.UserNotFoundException;
 import com.contestmodule.contest.dto.UserDto;
 import com.contestmodule.contest.entity.User;
 import com.contestmodule.contest.service.UserService;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.PermitAll;
 import javax.annotation.security.RolesAllowed;
+import javax.validation.Valid;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -35,14 +38,21 @@ public class UserController {
 
     @PermitAll
     @PostMapping("/create")
-    public User createUser(@RequestBody User user) {
+    public User createUser(@Valid @RequestBody User user) {
         logger.info("createUser() was called with username: " + user.getEmail());
-        return userService.save(user, false);
+        if (userService.getUserByUsername(user.getEmail()) != null) {
+            throw new UserAlreadyExistsException("This email is already registered!");
+        } else
+            return userService.save(user, false);
     }
 
     @GetMapping("/id/{id}")
     public Optional<User> findUserById(@PathVariable Long id) {
-        return userService.findUserById(id);
+        Optional<User> user = userService.findUserById(id);
+        if (user.isPresent())
+            return user;
+        else
+            throw new UserNotFoundException("This user does not exist!");
     }
 
     @DeleteMapping("/delete")
@@ -50,26 +60,5 @@ public class UserController {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String username = auth.getName();
         userService.deleteUser(username);
-    }
-
-
-    @GetMapping("/403")
-    public String error403(){
-        return "403";
-    }
-
-    @GetMapping(value = "/any-page", produces = MediaType.TEXT_HTML_VALUE)
-    @ResponseBody
-    public String returnSingleHTTPpage(){
-
-        String path = "user/src/main/resources/templates/anyPage.html";
-        String content = "dummyText";
-        try {
-            content = Files.readString(Paths.get(path));
-            System.out.println(content);
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-        return content;
     }
 }
