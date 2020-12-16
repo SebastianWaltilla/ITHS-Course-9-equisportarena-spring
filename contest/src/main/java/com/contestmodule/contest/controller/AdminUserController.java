ed65349cc4de4c61a1483e4c83d534b7824e6fbe
Linus Selvén <l.selven97@gmail.com>
2020-12-16 11:50:00 +0100

    Added exception handling to AdminUserController. Added findAll to UserRepository that returns a List. Created another delete method in UserService that deletes an entity based on an id.

--- contest/src/main/java/com/contestmodule/contest/controller/AdminUserController.java
+++ contest/src/main/java/com/contestmodule/contest/controller/AdminUserController.java
@@ -1,15 +1,22 @@
 package com.contestmodule.contest.controller;
 
+import com.contestmodule.contest.Exceptions.EmptyListException;
+import com.contestmodule.contest.Exceptions.UserAlreadyExistsException;
+import com.contestmodule.contest.Exceptions.UserNotFoundException;
+import com.contestmodule.contest.dto.UserNoPasswordDto;
 import com.contestmodule.contest.entity.User;
 import com.contestmodule.contest.service.UserService;
+import org.apache.coyote.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.RolesAllowed;
 import javax.validation.Valid;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -27,19 +34,31 @@ public class AdminUserController {
     }
 
     @GetMapping("/find-all")
-    public Iterable<User> findAllUsers() {
-        return userService.findAllUsers();
+    public ResponseEntity<List<User>> findAllUsers() {
+        List<User> users = userService.findAllUsers();
+
+        if (users.size() == 0)
+            throw new EmptyListException("No users found.");
+
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
     @PostMapping("/register-admin")
-    public User createAdminUser(@RequestBody @Valid User user) {
+    public UserNoPasswordDto createAdminUser(@RequestBody @Valid User user) {
         logger.info("CreateAdminUser() was called with the email" + user.getEmail());
-        return userService.save(user, true);
+        if (userService.getUserByUsername(user.getEmail()) != null) {
+            throw new UserAlreadyExistsException("This email is already registered!");
+        } else
+            return new UserNoPasswordDto().getUserNoPasswordDtoFromUser(userService.save(user, true));
     }
 
-    @DeleteMapping("delete/{username}")
-    public void deleteUser(@PathVariable String username) {
-        userService.deleteUser(username);
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity deleteUser(@PathVariable Long id) {
+        if (userService.findUserById(id) != null) {
+            userService.deleteUser(id);
+            return new ResponseEntity(id + " was deleted", HttpStatus.OK);
+        } else
+            throw new UserNotFoundException("User not found.");
     }
 
 
