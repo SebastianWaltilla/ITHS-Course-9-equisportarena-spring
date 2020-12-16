ff671af646aaff18f6b4c9e204f3c732f3c872d8
Linus Selvén <l.selven97@gmail.com>
2020-12-16 11:16:52 +0100

    Refactoring name to better suit the exception thrown.

---
+++ contest/src/main/java/com/contestmodule/contest/Exceptions/EmptyListException.java
@@ -0,0 +1,8 @@
+package com.contestmodule.contest.Exceptions;
+
+public class EmptyListException extends RuntimeException{
+
+    public EmptyListException(String message) {
+        super(message);
+    }
+}
