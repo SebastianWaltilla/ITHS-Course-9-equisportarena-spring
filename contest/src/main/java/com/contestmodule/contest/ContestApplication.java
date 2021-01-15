00b713970ff98d8b010c83afe0d566d58f9bf469
Johanna <svallingson@gmail.com>
2021-01-15 13:38:58 +0100

    sending mail asynchronously

--- contest/src/main/java/com/contestmodule/contest/ContestApplication.java
+++ contest/src/main/java/com/contestmodule/contest/ContestApplication.java
@@ -12,12 +12,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @SpringBootApplication
+@EnableAsync
 public class ContestApplication {
 
     public static void main(String[] args) {
