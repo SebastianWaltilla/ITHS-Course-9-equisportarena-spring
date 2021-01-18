06b6adb6d8d3a20da026cc5b5f7a0905ad0fbe41
Johanna <svallingson@gmail.com>
2021-01-18 13:20:09 +0100

    added endpoint entry/my-entries

--- contest/src/main/java/com/contestmodule/contest/repository/EntryRepository.java
+++ contest/src/main/java/com/contestmodule/contest/repository/EntryRepository.java
@@ -2,6 +2,7 @@ package com.contestmodule.contest.repository;
 
 import com.contestmodule.contest.dto.AdminEntryDto;
 import com.contestmodule.contest.dto.ContestInfoForUserDto;
+import com.contestmodule.contest.dto.EntryWithContestNameDto;
 import com.contestmodule.contest.entity.Contest;
 import com.contestmodule.contest.entity.Entry;
 import com.contestmodule.contest.entity.User;
@@ -31,4 +32,13 @@ public interface EntryRepository extends CrudRepository <Entry, Long> {
     @Query(value = "SELECT e from Entry e where e.userId = :userId AND e.contest.id = :contestId")
     Optional<Entry> findByUserIdAndContestId(Long userId, Long contestId);
 
+    @Query(value = "SELECT new com.contestmodule.contest.dto.EntryWithContestNameDto(" +
+            "e.userId, " +
+            "e.contest.id, " +
+            "e.videolink, " +
+            "e.userComment, " +
+            "e.horseName, " +
+            "e.contest.name) " +
+            "from Entry e where e.userId = :userId")
+    Iterable<EntryWithContestNameDto> findEntriesByUserId(Long userId);
 }
